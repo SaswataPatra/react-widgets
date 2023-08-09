cdc2a80298aa45de07be0ff82e8d457abec2c28c
PoMaMa69 <saswatapatra15@gmail.com>
2023-08-10 00:04:29 +0530

    org refactor

---
+++ src/pages/ButtonPage.js
@@ -0,0 +1,39 @@
+import Button from "./components/Button";
+import { HiAnnotation } from "react-icons/hi";
+import "../index.css";
+function ButtonPage(params) {
+  return (
+    <div>
+      <div>
+        <Button primary rounded outline className="ml-5">
+          <HiAnnotation></HiAnnotation>Click me !
+        </Button>
+      </div>
+      <div>
+        <Button secondary outline>
+          <HiAnnotation></HiAnnotation>
+          save me !
+        </Button>
+      </div>
+      <div>
+        <Button success outline>
+          <HiAnnotation></HiAnnotation>
+          success
+        </Button>
+      </div>
+      <div>
+        <Button warning>
+          <HiAnnotation></HiAnnotation> download
+        </Button>
+      </div>
+      <div>
+        <Button danger>
+          {" "}
+          <HiAnnotation></HiAnnotation>danger !
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default ButtonPage;
